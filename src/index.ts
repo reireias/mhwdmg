5c179a0b08110ae9b2fd8aa22bd184015ab16105
reireias <reireias@gmail.com>
2019-09-01 23:14:53 +0900

    update

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,34 @@
+// TODO: check purple rate in IB
+const PHYSICAL_SHARPNESS_RATE: { [key: string]: number } = {
+  blue: 1.20,
+  green: 1.05,
+  orange: 0.75,
+  purple: 1.39,
+  red: 0.5,
+  white: 1.32,
+  yellow: 1.0
+}
+
 export function expectedValue(value: number): number {
   return value * 2
 }
 
+function physicalDamage(weapon: IWeapon, target: ITarget, motion: IMotion): number {
+  // モーション値 * 武器倍率 / 100 * 会心補正 * 斬れ味補正 * 怒り補正 * 肉質 / 100
+  // motion * attack / 100 * affinityRate * sharpnessRate * angerRate * physicalEffectiveness / 100
+  const affinityRate: number = 1.0
+  const sharpnessRate: number = PHYSICAL_SHARPNESS_RATE[weapon.sharpness]
+  const angerRate: number = 1.0
+  const result = motion.value * weapon.attack / 100 * affinityRate * sharpnessRate * angerRate * target.physicalEffectiveness / 100
+  return Math.round(result)
+}
+
+function elementalDamage(weapon: IWeapon, target: ITarget): number {
+  // 属性値 / 10 * 属性補正 * 斬れ味補正 * 怒り補正 * 肉質 / 100
+  // element / 10 * elementRate * sharpnessRate * angerRate * elementalEffectiveness / 100
+  return Math.round(weapon.element / 10 * target.elementalEffectiveness / 100)
+}
+
 export function damage(weapon: IWeapon, target: ITarget, motion: IMotion): number {
-  const physical = weapon.attack * target.physicalEffectiveness / 100 * motion.value / 100
-  const elemental = weapon.element * target.elementalEffectiveness / 100
-  return physical + elemental
+  return physicalDamage(weapon, target, motion) + elementalDamage(weapon, target)
 }
--- test/damage.test.ts
+++ test/damage.test.ts
@@ -1,19 +1,35 @@
 import { damage } from '@/index.ts'
 
-describe('damage', (): void => {
-  it('damage test dummy', (): void => {
+describe('physicalDamage', (): void => {
+  describe('sharpness', (): void => {
     const weapon: IWeapon = {
       affinity: 0,
       attack: 100,
-      element: 0
+      element: 0,
+      sharpness: 'white'
     }
     const target: ITarget = {
-      elementalEffectiveness: 10,
-      physicalEffectiveness: 50
+      anger: false,
+      elementalEffectiveness: 0,
+      physicalEffectiveness: 100
     }
     const motion: IMotion = {
-      value: 30
+      value: 100
     }
-    expect(damage(weapon, target, motion)).toBe(15)
+    const sharpnessList: Array<{ key: Sharpness; value: number }> = [
+      { key: 'red', value: 0.5 },
+      { key: 'orange', value: 0.75 },
+      { key: 'yellow', value: 1.0 },
+      { key: 'green', value: 1.05 },
+      { key: 'blue', value: 1.2 },
+      { key: 'white', value: 1.32 },
+      { key: 'purple', value: 1.39 }
+    ]
+    it('sharpness rate is correct', (): void => {
+      sharpnessList.forEach((sharpness: { key: Sharpness, value: number }) => {
+        weapon.sharpness = sharpness.key
+        expect(damage(weapon, target, motion)).toBe(100 * sharpness.value)
+      })
+    })
   })
 })
