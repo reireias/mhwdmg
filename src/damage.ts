a9cf47fc5a2189352751fb57ad5157f92398ec30
reireias <reireias@gmail.com>
2019-10-07 14:23:34 +0900

    apply prettier

--- src/damage.ts
+++ src/damage.ts
@@ -2,7 +2,7 @@ import { IMotion, ITarget, IWeapon } from './types/mhwdmg'
 
 const PHYSICAL_SHARPNESS_RATE: { [key: string]: number } = {
   ammo: 1.0,
-  blue: 1.20,
+  blue: 1.2,
   green: 1.05,
   orange: 0.75,
   purple: 1.39,
@@ -23,30 +23,62 @@ const ELEMENTAL_SHARPNESS_RATE: { [key: string]: number } = {
 
 function calcAffinity(baseDamage: number, weapon: IWeapon): number {
   const affinityRate: number = 1.25
-  const plusAffinity: number = Math.round(baseDamage * affinityRate) * Math.max(0, weapon.affinity) / 100
-  const minusAffinity: number = Math.round(baseDamage * 0.75) * Math.max(0, -weapon.affinity) / 100
-  const normalAffinity: number = Math.round(baseDamage) * (100 - Math.max(0, weapon.affinity) - Math.max(0, -weapon.affinity)) / 100
+  const plusAffinity: number =
+    (Math.round(baseDamage * affinityRate) * Math.max(0, weapon.affinity)) / 100
+  const minusAffinity: number =
+    (Math.round(baseDamage * 0.75) * Math.max(0, -weapon.affinity)) / 100
+  const normalAffinity: number =
+    (Math.round(baseDamage) *
+      (100 - Math.max(0, weapon.affinity) - Math.max(0, -weapon.affinity))) /
+    100
   return plusAffinity + minusAffinity + normalAffinity
 }
 
-function physicalDamage(weapon: IWeapon, target: ITarget, motion: IMotion): number {
+function physicalDamage(
+  weapon: IWeapon,
+  target: ITarget,
+  motion: IMotion
+): number {
   // モーション値 * 武器倍率 / 100 * 会心補正 * 斬れ味補正 * 怒り補正 * 肉質 / 100
   // motion * attack / 100 * affinityRate * sharpnessRate * angerRate * physicalEffectiveness / 100
   const sharpnessRate: number = PHYSICAL_SHARPNESS_RATE[weapon.sharpness]
   const angerRate: number = target.anger ? 1.1 : 1.0
-  const baseDamage = motion.value * weapon.attack / 100 * sharpnessRate * angerRate * target.physicalEffectiveness / 100
+  const baseDamage =
+    (((motion.value * weapon.attack) / 100) *
+      sharpnessRate *
+      angerRate *
+      target.physicalEffectiveness) /
+    100
   return calcAffinity(baseDamage, weapon)
 }
 
-function elementalDamage(weapon: IWeapon, target: ITarget, elementRate?: number): number {
+function elementalDamage(
+  weapon: IWeapon,
+  target: ITarget,
+  elementRate?: number
+): number {
   // 属性値 / 10 * 属性補正 * 斬れ味補正 * 怒り補正 * 肉質 / 100
   // element / 10 * elementRate * sharpnessRate * angerRate * elementalEffectiveness / 100
   const sharpnessRate: number = ELEMENTAL_SHARPNESS_RATE[weapon.sharpness]
   const angerRate: number = target.anger ? 1.1 : 1.0
   const elementRateValue = elementRate === undefined ? 1 : elementRate
-  return Math.round(weapon.element / 10 * elementRateValue * sharpnessRate * angerRate * target.elementalEffectiveness / 100)
+  return Math.round(
+    ((weapon.element / 10) *
+      elementRateValue *
+      sharpnessRate *
+      angerRate *
+      target.elementalEffectiveness) /
+      100
+  )
 }
 
-export function damage(weapon: IWeapon, target: ITarget, motion: IMotion): number {
-  return physicalDamage(weapon, target, motion) + elementalDamage(weapon, target, motion.elementRate)
+export function damage(
+  weapon: IWeapon,
+  target: ITarget,
+  motion: IMotion
+): number {
+  return (
+    physicalDamage(weapon, target, motion) +
+    elementalDamage(weapon, target, motion.elementRate)
+  )
 }
